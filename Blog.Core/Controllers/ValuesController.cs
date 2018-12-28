680503ef049c4d1f4253158c22c1636433b6e980
1961117443 <1961117443@qq.com>
2018-12-28 11:18:10 +0800

    测试

--- Blog.Core/Controllers/ValuesController.cs
+++ Blog.Core/Controllers/ValuesController.cs
@@ -40,7 +40,11 @@ namespace Blog.Core.Controllers
         //public void Post([FromBody] string value)
         //{
         //}
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
